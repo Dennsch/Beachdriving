70dd307582de4f652dea515e5d1066e726fe34b5
Dennsch <dennis.schall@compono.com>
2025-08-29 12:11:54 +1000

    test

---
+++ api/location.js
@@ -0,0 +1,41 @@
+export default async function handler(req, res) {
+  // Enable CORS
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  );
+
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+
+  const { locationId } = req.query;
+  
+  try {
+    const API_KEY = "ZWY2Y2FlZmZlNzQ0NzZhYzI4ZDNiNG";
+    const targetUrl = `https://api.willyweather.com.au/v2/${API_KEY}/locations/${locationId}.json`;
+    
+    const response = await fetch(targetUrl, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      }
+    });
+
+    const data = await response.json();
+    
+    if (!response.ok) {
+      return res.status(response.status).json({ error: data });
+    }
+
+    res.status(200).json(data);
+  } catch (error) {
+    console.error('Location API Error:', error);
+    res.status(500).json({ error: 'Failed to fetch location data' });
+  }
+}
